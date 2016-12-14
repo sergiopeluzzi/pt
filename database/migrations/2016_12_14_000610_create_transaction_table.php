82e13c0dc4d05ee23561c38dbdb2dbbe4a1293b9
Sergio Peluzzi <sergiopeluzzi@gmail.com>
2016-12-13 20:23:26 -0400

    adding some migrations

---
+++ database/migrations/2016_12_14_000610_create_transaction_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('from')->unsigned();
+            $table->foreign('from')->references('id')->on('users');
+            $table->integer('to')->unsigned();
+            $table->foreign('to')->references('id')->on('users');
+            $table->string('history');
+            $table->timestamp('date');
+            $table->float('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('transactions');
+    }
+}
